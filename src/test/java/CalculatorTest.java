9fcf36f28666e30f2449aa33ec2f0a9f5acfbdb6
Gamer1120 <michaelkoopmantest@gmail.com>
2018-12-04 12:11:56 +0100

    Add JUnit and a failing test to see whether Travis fails the build

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+	@Test
+	public void addTest() {
+		assertEquals(Calculator.add(1, 1), 3);
+	}
+}
